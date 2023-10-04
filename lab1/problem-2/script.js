31fdcea48cfdc6fd6591306e1e6c49416eb057da
DDavitaa <c20406272@mytudublin.ie>
2023-10-04 10:40:42 +0100

    added necessary functions in js

---
+++ lab1/problem-2/script.js
@@ -0,0 +1,29 @@
+function getNotes()
+{
+
+}
+
+function saveNotes(notes) 
+{
+
+}
+
+function createNote(id, content) 
+{
+
+}
+
+function addNotes() 
+{
+
+}
+
+function updateNote(id, newContent)
+{
+
+}
+
+function deleteNote(id, element)
+{
+
+}
