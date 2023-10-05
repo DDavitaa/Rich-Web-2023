fdccb93cfa2c48d5539c39a535cc06087cc23e91
DDavitaa <c20406272@mytudublin.ie>
2023-10-05 12:38:15 +0100

    added comments, added update functionality

--- lab1/problem-2/script.js
+++ lab1/problem-2/script.js
@@ -1,6 +1,11 @@
-const notesContainer = document.getElementById("note-app");
-const addNoteButton = notesContainer.querySelector(".add-Note");
 
+// represents the container app
+const notesContainer = document.getElementById("note-app");
+
+// refers to the add-note button
+const addNoteButton = notesContainer.querySelector(".add-note");
+
+// displays to user when called
 getNotes().forEach(note => {
     const noteElement = createNote(note.id, note.content);
 
@@ -9,28 +14,35 @@ getNotes().forEach(note => {
 
 addNoteButton.addEventListener("click", () => addNote());
 
+// gets all notes
 function getNotes()
 {
     return JSON.parse(localStorage.getItem("note-app_notes") || "[]");
 }
 
+// saves notes to local storage
 function saveNotes(notes) 
 {
     localStorage.setItem("note-app_notes", JSON.stringify(notes));
 }
 
+// Builds new element to represent a new note
 function createNote(id, content) 
 {
+    // creates a new textarea element
     const element = document.createElement("textarea");
 
+    // adds 'note' to classList for CSS to recognise
     element.classList.add("note");
     element.value = content;
     element.placeholder = "Empty note";
 
+    // when changed, then call update function
     element.addEventListener("change", () => {
         updateNote(id, element.value);
     })
 
+    // when double clicked, then call delete function
     element.addEventListener("dblclick", () => {
         const doDelete = confirm("Are you sure you want to delete this note?");
 
@@ -43,27 +55,41 @@ function createNote(id, content)
     return element;
 }
 
+// added new note to html
 function addNote() 
 {
     const notes = getNotes();
+
+    // sets id to random number
     const noteObject = {
         id: Math.floor(Math.random() * 100000),
         content: ""
     };
 
+    // creates new note element
     const noteElement = createNote(noteObject.id, noteObject.content);
+
+    // puts note before the add note button
     notesContainer.insertBefore(noteElement, addNoteButton);
 
     notes.push(noteObject);
     saveNotes(notes);
 }
 
+// editing the note
 function updateNote(id, newContent)
 {
+    const notes = getNotes();
 
+    // filters the note by id
+    const targetNote = notes.filter(note => note.id == id)[0];
+
+    targetNote.content = newContent;
+    saveNotes(notes);
 }
 
+// deletes the note
 function deleteNote(id, element)
 {
-
+    
 }
