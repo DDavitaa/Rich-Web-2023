dd0cba9a59fd5e22f6d9d3c46e61c89e15db2bfa
DDavitaa <c20406272@mytudublin.ie>
2023-10-10 00:20:16 +0100

    nearly done problem 3

---
+++ lab1/problem-3/Content.js
@@ -0,0 +1,76 @@
+//array of images
+let metalPipe_images = [
+    "https://res.cloudinary.com/rsc/image/upload/b_rgb:FFFFFF,c_pad,dpr_2.625,f_auto,h_535,q_auto,w_950/c_pad,h_535,w_950/R0190844-01?pgw=1&pgwact=1",
+    "https://www.kloecknermetals.com/wp-content/uploads/2017/08/70______-Kopie.jpg",
+    "https://i.ebayimg.com/images/g/ImIAAOSwjytdUCyo/s-l1200.jpg",
+    "https://i.ebayimg.com/images/g/kIYAAOSw8tZhSs6g/s-l1600.jpg",
+	"https://www.reliance-foundry.com/wp-content/uploads/steel-pipe.jpg",
+    "https://i.makeagif.com/media/2-19-2023/k3unyo.gif",
+    "https://steamuserimages-a.akamaihd.net/ugc/2023843663332715309/C93F2040476F4845F66A5888B9206236B8EED0FC/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
+    "https://www.steelandpipes.com/cdn/shop/files/Pipes_540x540.gif?v=1675360229"
+];
+
+window.onload = function() {
+    // changes background image
+    document.body.style.backgroundImage = `url(${metalPipe_images[Math.floor(Math.random() * metalPipe_images.length)]})`;
+
+    // changes all images
+    const imgs = document.getElementsByTagName("img");
+    for(let i = 0; i < imgs.length; i++) {
+        const randomImg = Math.floor(Math.random() * metalPipe_images.length)
+        imgs[i].src = metalPipe_images[randomImg]
+    }
+
+    // changes all h1
+    const headers = document.getElementsByTagName("h1");
+    for (let i = 0; i < headers.length; i++){
+        headers[i].innerText = "METAL PIPE IS TAKING OVER";
+    }
+
+    // changes all h2
+    const headers2 = document.getElementsByTagName("h2");
+    for (let i = 0; i < headers2.length; i++){
+        headers2[i].innerText = "I LOVE METAL PIPE";
+    }
+
+    // changes all p
+    const p = document.getElementsByTagName("p");
+    for (let i = 0; i < p.length; i++){
+        p[i].innerText = "METAL PIPE";
+    }
+
+    // changes all buttons
+    const buttons = document.getElementsByTagName("button");
+    for (let i = 0; i < buttons.length; i++){
+        buttons[i].innerText = "PRESS FOR METAL PIPE";
+        buttons[i].style.backgroundImage = `url(${metalPipe_images[Math.floor(Math.random() * metalPipe_images.length)]})`;
+        buttons[i].style.color = "black";
+    }
+
+    // changes all links
+    const links = document.getElementsByTagName("a");
+    for (let i = 0; i < links.length; i++){
+        links[i].href = "https://www.metalpipe.com";
+    }
+
+    const li = document.getElementsByTagName("li");
+    for (let i = 0; i < li.length; i++){
+        li[i].innerText = "METAL BALL ";
+    }
+
+    const divs = document.querySelectorAll("div");
+    for (let i = 0; i < divs.length; i++){
+        divs[i].style.backgroundColor = "transparent";
+    }
+
+    // Create an audio element
+    const audio = new Audio("https://tuna.voicemod.net/sound/ade71f0d-a41b-4e3a-8097-9f1cc585745c");
+
+    // Play the audio when the page finishes loading
+    window.addEventListener("load", function() {
+    audio.play();
+    });
+
+    
+    
+}
---
+++ lab1/problem-3/readme.md
@@ -0,0 +1,16 @@
+# Problem 3 
+
+I changed the theme from cats to metal pipes.
+Metal pipes are a meme as they are considered funny for no reason.
+
+## Changelog
+
+These are the changes:
+
+- Changed name of extension in manifest.
+- Changed images in array.
+- Made sure to run everything after all initial scripts from the webpages have been loaded.
+- Changed background to just metal pipes.
+- Changed what h1 says
+
+
