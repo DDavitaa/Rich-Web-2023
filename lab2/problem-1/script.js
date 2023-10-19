351941deb1ecb2e84a4b33f293def8854292bc45
DDavitaa <c20406272@mytudublin.ie>
2023-10-19 20:01:21 +0100

    working on adding contact

--- lab2/problem-1/script.js
+++ lab2/problem-1/script.js
@@ -1,9 +1,80 @@
-// Get a reference to the form element
+// constant variables
 const form = document.getElementById('phone_dir_form');
+const table = document.getElementById('tableBody');
 const errorMessage = document.getElementById('error');
 const noResultMessage = document.getElementById('no_result');
 
-// addBtn.addEventListener("click", () => addContact());
+getContacts().forEach(contact => {
+    const rowContact = createNote(contact.id, contact.name, contact.phone, contact.email);
+});
+
+function getContacts()
+{
+    return JSON.parse(localStorage.getItem("note-phoneDir_app") || "[]");
+}
+
+function saveContacts(notes) 
+{
+    localStorage.setItem("phoneDir_app", JSON.stringify(notes));
+}
+
+function validateContact(name, phone, email)
+{
+    const patterName = /^[A-Za-z ]{1,20}$/;
+    const patternPhone = /^[0-9]{10}$/;
+    const patternEmail = /^[^\s@]+@[^\s@]+\.[^\s@]{1,40}$/
+
+    console.log(name, phone, email);
+
+    if(patterName.test(name) && patternPhone.test(phone) && patternEmail.test(email))
+    {
+        errorMessage.style.display = "none";
+        console.log("valid");
+    }
+    else
+    {
+        errorMessage.style.display = "block";
+    }
+}
+
+function createContact(id, name, phone, email)
+{
+    var newRow = document.createElement('tr');
+
+    // Create new table data elements for each contact property
+    var idData = document.createElement('td');
+    idData.textContent = id;
+    var nameData = document.createElement('td');
+    nameData.textContent = name;
+    var phoneData = document.createElement('td');
+    phoneData.textContent = phone;
+    var emailData = document.createElement('td');
+    emailData.textContent = email;
+
+    // Append the table data elements to the new row element
+    newRow.appendChild(idData);
+    newRow.appendChild(nameData);
+    newRow.appendChild(phoneData);
+    newRow.appendChild(emailData);
+
+    // Append the new row element to the contacts table body element
+    tableBody.appendChild(newRow);
+}
+
+function addContact()
+{
+    const contact = getContacts();
+
+    const contactObj = {
+        id: Math.floor(Math.random() * 100000),
+        name: "",
+        phone: "",
+        email: ""
+    };
+
+    createContact(contactObj.id, contactObj.name, contactObj.phone, contactObj.email);
+}
+
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
@@ -16,26 +87,6 @@ form.addEventListener('submit', function(event) {
     let phone = phoneInput.value;
     let email = emailInput.value;
 
-    let patterName = /^[A-Za-z ]{1,20}$/;
-    let patternPhone = /^[0-9]{10}$/;
-    let patternEmail = /^[^\s@]+@[^\s@]+\.[^\s@]{1,40}$/
-
-    console.log(name, phone, email);
-
-    // if(name == "" || phone == "" || email == "")
-    // {
-    //     errorMessage.style.display = "block";
-    // }
-
-    if(patterName.test(name) && patternPhone.test(phone) && patternEmail.test(email))
-    {
-        errorMessage.style.display = "none";
-        console.log("valid");
-    }
-    else
-    {
-        errorMessage.style.display = "block";
-    }
-
-
+    validateContact(name, phone, email);
+    
 });
