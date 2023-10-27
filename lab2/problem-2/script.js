004cb0257aa999633e02474924388fa3afcc936c
DDavitaa <c20406272@mytudublin.ie>
2023-10-27 22:41:15 +0100

    problem 2 task 2 done

--- lab2/problem-2/script.js
+++ lab2/problem-2/script.js
@@ -11,4 +11,11 @@ fetch('https://jsonplaceholder.typicode.com/posts')
         const filteredPosts = json.filter(checkTitle);
         console.log("task 1:", filteredPosts)
 
+        // Task 2
+        const wordFrequency = json.flatMap(post => post.body.split(/\W+/))
+            .reduce((freqMap, word) => ({
+                ...freqMap,
+                [word]: (freqMap[word] || 0) + 1
+            }), {});
+        console.log("task 2:", wordFrequency);
     })
