82def37738b10244cfc4a0a0ca140bb192e237bd
DDavitaa <c20406272@mytudublin.ie>
2023-11-25 21:37:23 +0000

    showing on display now

--- lab3/problem-2/script.js
+++ lab3/problem-2/script.js
@@ -3,9 +3,19 @@ const pause_resume_btn = document.getElementById("pause_resume");
 const hour_input = document.getElementById("hours");
 const min_input = document.getElementById("minutes");
 const sec_input = document.getElementById("seconds");
+const countdown = document.getElementById("countdown");
 
 const start$ = rxjs.fromEvent(start_btn, "click");
 const pauseResume$ = rxjs.fromEvent(pause_resume_btn, "click");
+
+const inputs = [hour_input, min_input, sec_input];
+
+// prevents entry of - and . in the input fields
+inputs.forEach(input => {
+    rxjs.fromEvent(input, 'input').subscribe((event) => {
+        event.target.value = event.target.value.replace(/[-.]/g, '');
+    });
+});
 
 const isPaused$ = new rxjs.BehaviorSubject(false);
 let elapsedSeconds = 0;
@@ -14,41 +24,88 @@ pauseResume$.subscribe(() => {
     isPaused$.next(!isPaused$.value);
 });
 
+// start$.pipe(
+//     rxjs.switchMap(() => {
+//         elapsedSeconds = 0;
+//         const hour$ = rxjs.fromEvent(hour_input, "input").pipe(
+//             rxjs.map(event => Number(event.target.value)),
+//             rxjs.startWith(hour_input.value ? Number(hour_input.value) : 0)
+//         );
+
+//         const min$ = hour$.pipe(
+//             rxjs.switchMap(() => rxjs.fromEvent(min_input, "input").pipe(
+//                 rxjs.map(event => Number(event.target.value)),
+//                 rxjs.startWith(min_input.value ? Number(min_input.value) : 0)
+//             ))
+//         );
+
+//         const sec$ = min$.pipe(
+//             rxjs.switchMap(() => rxjs.fromEvent(sec_input, "input").pipe(
+//                 rxjs.map(event => Number(event.target.value)),
+//                 rxjs.startWith(sec_input.value ? Number(sec_input.value) : 0)
+//             ))
+//         );
+
+//         const countdownTime$ = rxjs.combineLatest([hour$, min$, sec$]).pipe(
+//             rxjs.map(([hours, minutes, seconds]) => hours * 3600 + minutes * 60 + seconds)
+//         );
+
+//         const countdown$ = countdownTime$.pipe(
+//             rxjs.switchMap(totalSeconds => {
+//                 return rxjs.interval(1000).pipe(
+//                     rxjs.filter(() => !isPaused$.value),
+//                     rxjs.map(i => {
+//                         elapsedSeconds++;
+//                         return totalSeconds - elapsedSeconds;
+//                     }),
+//                     rxjs.take(totalSeconds + 1),
+//                     rxjs.takeUntil(start$)
+//                 );
+//             })
+//         );
+
+//         return countdown$;
+//     })
+// ).subscribe(remainingSeconds => {
+//     console.log(remainingSeconds);
+// });
+
 start$.pipe(
     rxjs.switchMap(() => {
         elapsedSeconds = 0;
-        const hour$ = rxjs.fromEvent(hour_input, "input").pipe(
+        const sec$ = rxjs.fromEvent(sec_input, "input").pipe(
             rxjs.map(event => Number(event.target.value)),
-            rxjs.startWith(hour_input.value ? Number(hour_input.value) : 0)
+            rxjs.startWith(sec_input.value ? Number(sec_input.value) : 0)
         );
 
-        const min$ = hour$.pipe(
+        const min$ = sec$.pipe(
             rxjs.switchMap(() => rxjs.fromEvent(min_input, "input").pipe(
                 rxjs.map(event => Number(event.target.value)),
                 rxjs.startWith(min_input.value ? Number(min_input.value) : 0)
             ))
         );
 
-        const sec$ = min$.pipe(
-            rxjs.switchMap(() => rxjs.fromEvent(sec_input, "input").pipe(
+        const hour$ = min$.pipe(
+            rxjs.switchMap(() => rxjs.fromEvent(hour_input, "input").pipe(
                 rxjs.map(event => Number(event.target.value)),
-                rxjs.startWith(sec_input.value ? Number(sec_input.value) : 0)
+                rxjs.startWith(hour_input.value ? Number(hour_input.value) : 0)
             ))
         );
 
         const countdownTime$ = rxjs.combineLatest([hour$, min$, sec$]).pipe(
-            rxjs.map(([hours, minutes, seconds]) => hours * 3600 + minutes * 60 + seconds)
+            rxjs.map(([hours, minutes, seconds]) => hours * 3600 + minutes * 60 + seconds + 1)
         );
 
         const countdown$ = countdownTime$.pipe(
             rxjs.switchMap(totalSeconds => {
                 return rxjs.interval(1000).pipe(
+                    rxjs.startWith(totalSeconds),
                     rxjs.filter(() => !isPaused$.value),
                     rxjs.map(i => {
                         elapsedSeconds++;
                         return totalSeconds - elapsedSeconds;
                     }),
-                    rxjs.take(totalSeconds + 1),
+                    rxjs.take(totalSeconds),
                     rxjs.takeUntil(start$)
                 );
             })
@@ -58,4 +115,14 @@ start$.pipe(
     })
 ).subscribe(remainingSeconds => {
     console.log(remainingSeconds);
+
+    const hours = Math.floor(remainingSeconds / 3600);
+    const minutes = Math.floor((remainingSeconds % 3600) / 60);
+    const seconds = remainingSeconds % 60;
+
+    const hours_show = hours < 10 ? `0${hours}` : `${hours}`;
+    const mins_show = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    const secs_show = seconds < 10 ? `0${seconds}` : `${seconds}`;
+
+    countdown.innerHTML = `${hours_show}:${mins_show}:${secs_show}`;
 });
---
+++ lab3/problem-2/script2.js
@@ -0,0 +1,96 @@
+
+const hour_input = document.getElementById("hours");
+const min_input = document.getElementById("minutes");
+const sec_input = document.getElementById("seconds");
+
+const start_btn = document.getElementById("start");
+const pause_resume_btn = document.getElementById("pause_resume");
+
+const countdown = document.getElementById("countdown");
+
+const inputs = [hour_input, min_input, sec_input];
+
+// prevents entry of - and . in the input fields
+inputs.forEach(input => {
+    rxjs.fromEvent(input, 'input').subscribe((event) => {
+        event.target.value = event.target.value.replace(/[-.]/g, '');
+    });
+});
+
+const hour$ = rxjs.fromEvent(hour_input, "input").pipe(
+    rxjs.map(event => Number(event.target.value))
+);
+const min$ = rxjs.fromEvent(min_input, "input").pipe(
+    rxjs.map(event => Number(event.target.value))
+);
+const sec$ = rxjs.fromEvent(sec_input, "input").pipe(
+    rxjs.map(event => Number(event.target.value))
+);
+
+const start$ = rxjs.fromEvent(start_btn, "click");
+const pause_resume$ = rxjs.fromEvent(pause_resume_btn, "click");
+
+const countdownTimeInSeconds = 300;
+
+const seconds$ = rxjs.interval(1000).pipe(
+    rxjs.map(i => countdownTimeInSeconds - i),
+    rxjs.take(countdownTimeInSeconds + 1)
+    //rxjs.takeUntil(pause_resume$)
+);
+
+// const pauseResumeClicks$ = pause_resume$.pipe(
+//     rxjs.startWith(false),
+//     rxjs.map(val => !val),
+//     rxjs.switchMap(paused => paused ? seconds$ : rxjs.EMPTY)
+// );
+
+
+// const pauseResumeClicks$ = pause_resume$.pipe(
+//     rxjs.startWith(false),
+//     rxjs.scan(val => !val),
+//     rxjs.filter(val => val)
+// );
+
+// const seconds$ = pauseResumeClicks$.pipe(
+//     rxjs.switchMap(paused => 
+//         paused 
+//         ? rxjs.interval(1000).pipe(
+//             rxjs.map(i => {
+//                 elapsedTime++;
+//                 return countdownTimeInSeconds - elapsedTime;
+//             }),
+//             rxjs.takeUntil(pauseResumeClicks$)
+//         )
+//         : rxjs.interval(0)
+//     )
+// );
+
+const minutes$ = seconds$.pipe(
+    rxjs.map(seconds => Math.floor(seconds / 60))
+);
+
+const hours$ = minutes$.pipe(
+    rxjs.map(minutes => Math.floor(minutes / 60))
+);
+
+
+
+hours$.subscribe(hours => {
+    const hours_show = hours < 10 ? `0${hours}` : `${hours}`;
+
+    countdown.innerHTML = `${hours_show}:`;
+});
+
+minutes$.subscribe(minutes => {
+    const mins_format = minutes % 60;
+    const mins_show = mins_format < 10 ? `0${mins_format}` : `${mins_format}`;
+
+    countdown.innerHTML += `${mins_show}:`;
+});
+
+seconds$.subscribe(seconds => {
+    const secs_format = seconds % 60;
+    const secs_show = secs_format < 10 ? `0${secs_format}` : `${secs_format}`;
+
+    countdown.innerHTML += `${secs_show}`;
+});
