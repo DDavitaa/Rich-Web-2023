731d697fe211dd3f47132f60d6dd669b774cdf82
DDavitaa <c20406272@mytudublin.ie>
2023-11-25 21:55:52 +0000

    finished lab 3 problem 2

--- lab3/problem-2/script.js
+++ lab3/problem-2/script.js
@@ -18,69 +18,36 @@ inputs.forEach(input => {
 });
 
 const isPaused$ = new rxjs.BehaviorSubject(false);
-let elapsedSeconds = 0;
+
+
+isPaused$.subscribe(() => {
+    pause_resume_btn.innerHTML = isPaused$.value ? "&#9658; RESUME" : "&#9612;&#9612; PAUSE";
+});
 
 pauseResume$.subscribe(() => {
-    pause_resume_btn.innerHTML = isPaused$.value ? "&#9612;&#9612; PAUSE" : "&#9658; RESUME";
+    
     isPaused$.next(!isPaused$.value);
 });
 
-// start$.pipe(
-//     rxjs.switchMap(() => {
-//         elapsedSeconds = 0;
-//         const hour$ = rxjs.fromEvent(hour_input, "input").pipe(
-//             rxjs.map(event => Number(event.target.value)),
-//             rxjs.startWith(hour_input.value ? Number(hour_input.value) : 0)
-//         );
-
-//         const min$ = hour$.pipe(
-//             rxjs.switchMap(() => rxjs.fromEvent(min_input, "input").pipe(
-//                 rxjs.map(event => Number(event.target.value)),
-//                 rxjs.startWith(min_input.value ? Number(min_input.value) : 0)
-//             ))
-//         );
-
-//         const sec$ = min$.pipe(
-//             rxjs.switchMap(() => rxjs.fromEvent(sec_input, "input").pipe(
-//                 rxjs.map(event => Number(event.target.value)),
-//                 rxjs.startWith(sec_input.value ? Number(sec_input.value) : 0)
-//             ))
-//         );
-
-//         const countdownTime$ = rxjs.combineLatest([hour$, min$, sec$]).pipe(
-//             rxjs.map(([hours, minutes, seconds]) => hours * 3600 + minutes * 60 + seconds)
-//         );
-
-//         const countdown$ = countdownTime$.pipe(
-//             rxjs.switchMap(totalSeconds => {
-//                 return rxjs.interval(1000).pipe(
-//                     rxjs.filter(() => !isPaused$.value),
-//                     rxjs.map(i => {
-//                         elapsedSeconds++;
-//                         return totalSeconds - elapsedSeconds;
-//                     }),
-//                     rxjs.take(totalSeconds + 1),
-//                     rxjs.takeUntil(start$)
-//                 );
-//             })
-//         );
-
-//         return countdown$;
-//     })
-// ).subscribe(remainingSeconds => {
-//     console.log(remainingSeconds);
-// });
+let elapsedSeconds = 0;
+let isRunning = false;
 
 start$.pipe(
+    rxjs.tap(() => {
+        isPaused$.next(false);
+        isRunning = true;
+    }),
     rxjs.switchMap(() => {
         elapsedSeconds = 0;
         const sec$ = rxjs.fromEvent(sec_input, "input").pipe(
+            rxjs.filter(() => !isRunning),
             rxjs.map(event => Number(event.target.value)),
             rxjs.startWith(sec_input.value ? Number(sec_input.value) : 0)
         );
 
         const min$ = sec$.pipe(
             rxjs.switchMap(() => rxjs.fromEvent(min_input, "input").pipe(
+                rxjs.filter(() => !isRunning),
                 rxjs.map(event => Number(event.target.value)),
                 rxjs.startWith(min_input.value ? Number(min_input.value) : 0)
             ))
@@ -88,6 +55,7 @@ start$.pipe(
 
         const hour$ = min$.pipe(
             rxjs.switchMap(() => rxjs.fromEvent(hour_input, "input").pipe(
+                rxjs.filter(() => !isRunning),
                 rxjs.map(event => Number(event.target.value)),
                 rxjs.startWith(hour_input.value ? Number(hour_input.value) : 0)
             ))
@@ -115,7 +83,9 @@ start$.pipe(
         return countdown$;
     })
 ).subscribe(remainingSeconds => {
-    console.log(remainingSeconds);
+    if (remainingSeconds <= 0) {
+        isRunning = false;
+    }
 
     const hours = Math.floor(remainingSeconds / 3600);
     const minutes = Math.floor((remainingSeconds % 3600) / 60);
