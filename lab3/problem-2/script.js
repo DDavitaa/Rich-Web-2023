aa2a9e5193da6a08feddfab4d35e0de4a1848d41
DDavitaa <c20406272@mytudublin.ie>
2023-11-25 22:32:43 +0000

    added iframe

--- lab3/problem-2/script.js
+++ lab3/problem-2/script.js
@@ -1,13 +1,18 @@
+
+// get DOM elements
 const start_btn = document.getElementById("start");
 const pause_resume_btn = document.getElementById("pause_resume");
 const hour_input = document.getElementById("hours");
 const min_input = document.getElementById("minutes");
 const sec_input = document.getElementById("seconds");
 const countdown = document.getElementById("countdown");
+const video = document.getElementById("alarm");
 
+// create observables from click events
 const start$ = rxjs.fromEvent(start_btn, "click");
 const pauseResume$ = rxjs.fromEvent(pause_resume_btn, "click");
 
+// array of input fields
 const inputs = [hour_input, min_input, sec_input];
 
 // prevents entry of - and . in the input fields
@@ -17,26 +22,30 @@ inputs.forEach(input => {
     });
 });
 
+// BehaviourSubject to keep track of whether the timer is paused or not
 const isPaused$ = new rxjs.BehaviorSubject(false);
 
-
+// subscribe to isPaused$ to change the text of the pause/resume button
 isPaused$.subscribe(() => {
     pause_resume_btn.innerHTML = isPaused$.value ? "&#9658; RESUME" : "&#9612;&#9612; PAUSE";
 });
 
+// subscribe to pauseResume$ to toggle the value of isPaused$
 pauseResume$.subscribe(() => {
-    
     isPaused$.next(!isPaused$.value);
 });
 
-let elapsedSeconds = 0;
+// variables to keep track of the elapsed time and whether the timer is running
 let isRunning = false;
 
+// subscribe to start$ to start the timer
 start$.pipe(
+    // when tapped, set isPaused$ to false and isRunning to true
     rxjs.tap(() => {
         isPaused$.next(false);
         isRunning = true;
     }),
+    // switchMap to a new observable that emits the remaining seconds
     rxjs.switchMap(() => {
         elapsedSeconds = 0;
         const sec$ = rxjs.fromEvent(sec_input, "input").pipe(
@@ -83,9 +92,9 @@ start$.pipe(
         return countdown$;
     })
 ).subscribe(remainingSeconds => {
-    if (remainingSeconds <= 0) {
-        isRunning = false;
-    }
+    //if (remainingSeconds <= 0) {
+    //    isRunning = false;
+    //}
 
     const hours = Math.floor(remainingSeconds / 3600);
     const minutes = Math.floor((remainingSeconds % 3600) / 60);
@@ -96,4 +105,25 @@ start$.pipe(
     const secs_show = seconds < 10 ? `0${seconds}` : `${seconds}`;
 
     countdown.innerHTML = `${hours_show}:${mins_show}:${secs_show}`;
+
+    if (remainingSeconds <= 0) {
+        video.style.display = "block";
+        player.playVideo();
+    }
+    else {
+        video.style.display = "none";
+    }
 });
+
+let player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('youtube_video', {
+        events: {
+            'onReady': onPlayerReady
+        }
+    });
+}
+
+function onPlayerReady(event) {
+
+}
