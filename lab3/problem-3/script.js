0da587e0e48f3a2e4587171f8ae4fa9d7c21d9f9
DDavitaa <c20406272@mytudublin.ie>
2023-11-26 18:33:59 +0000

    started html and js getting doms

---
+++ lab3/problem-3/script.js
@@ -0,0 +1,6 @@
+const input = document.querySelector("input");
+const create_btn = document.getElementById("create_note");
+const list_notes = document.getElementById("list_notes");
+const note = document.getElementsByClassName("note");
+const delete_btn = document.getElementsByClassName("delete_note");
+
