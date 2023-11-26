ad450ecb9c10da887ea305c1868c76914ece1a6b
DDavitaa <c20406272@mytudublin.ie>
2023-11-26 22:04:37 +0000

    deletion of children and descendants working

--- lab3/problem-3/script.js
+++ lab3/problem-3/script.js
@@ -1,35 +1,45 @@
 
+// get DOM elements
 const input = document.querySelector("input");
 const create_btn = document.getElementById("create_note");
 const error = document.getElementById("error");
 const error2 = document.getElementById("error2");
 const list_notes = document.getElementById("list_notes");
-const note = document.getElementsByClassName("note");
-const delete_btn = document.getElementsByClassName("delete_note");
 
+// create observable from input event
 const input$ = rxjs.fromEvent(input, "input").pipe(
+    // get the value of the input field
     rxjs.map(event => event.target.value)
 );
 
+// boolean to keep track of whether the note can be created or not
 let doCreate = true;
 
+// create observable from click event
 const create$ = rxjs.fromEvent(create_btn, "click").pipe(
+    // if doCreate is true, create the note
     rxjs.filter(() => doCreate)   
 );
-//const delete$ = rxjs.fromEvent(delete_btn, "click");
-const delete$ = rxjs.fromEvent(delete_btn, "click").pipe(
-    rxjs.map(event => event.target.id)
-);
 
+// subject to keep track of delete button clicks
+const deleteButtons$ = new rxjs.Subject();
+
+// array to keep track of notes
 let notes = [];
+
+// id of the next note to be created
 let id = 1;
 
+// subscribe to input$
 input$.subscribe((value) => {
-    if(id == 1) {
+    // if the input field is empty, display cannot assign parent to first note
+    if(notes.length == 0) {
+        // if there is a value in the input field, display error and don't allow create note
         if (value) {
             error.style.display = "block";
             doCreate = false;
         }
+        // if the input field is empty, hide error and allow create note
         else if(!value) {
             error.style.display = "none";
             doCreate = true;
@@ -40,7 +50,6 @@ input$.subscribe((value) => {
 
         let found = false;
         for(let i = 0; i < notes.length; i++) {
-            console.log(notes[i].id);
             if(value == notes[i].id || value == "") {
                 found = true;
                 break;
@@ -71,6 +80,7 @@ create$.subscribe(() => {
 
 function createNote(newNote, number) {
     const divNote = document.createElement("div");
+    divNote.id = newNote.id;
     divNote.classList.add("note");
     const pNote = document.createElement("p");
     if (number) {
@@ -82,31 +92,54 @@ function createNote(newNote, number) {
     const deleteNote = document.createElement("button");
     deleteNote.classList.add("delete_note");
     deleteNote.innerHTML = "Delete";
-    deleteNote.id = newNote.id;
+
+    const deleteButton$ = rxjs.fromEvent(deleteNote, 'click').pipe(
+        rxjs.map(() => newNote.id)
+    );
+
+    deleteButtons$.next(deleteButton$);
 
     divNote.appendChild(pNote);
     divNote.appendChild(deleteNote);
     list_notes.appendChild(divNote);
 }
 
+const delete$ = deleteButtons$.pipe(
+    rxjs.mergeAll()
+);
+
+
+delete$.subscribe(id => {
+    console.log(`Deleting note with id: ${id}`);
+    deleteNoteAndDescendants(id);
+});
+
+function deleteNoteAndDescendants(id) {
+    // Find and delete the note with the given id
+    const noteIndex = notes.findIndex(note => note.id === id);
+    if (noteIndex !== -1) {
+        notes.splice(noteIndex, 1);
+        const element = document.getElementById(id);
+        if (element) {
+            element.remove();
+        } else {
+            console.log(`Element with id: ${id} not found in the DOM.`);
+        }
+    } else {
+        console.log(`Note with id: ${id} not found in the notes array.`);
+    }
+
+    // Find any notes that have the deleted note as their parent
+    const childNotes = notes.filter(note => note.parentID === id);
+    childNotes.forEach(childNote => {
+        // Recursively delete the child note and its descendants
+        deleteNoteAndDescendants(childNote.id);
+    });
+}
 
 class Note {
     constructor(id, parentID) {
         this.id = id;
         this.parentID = parentID;
-
-        delete$.subscribe((deletedId) => {
-            console.log("delete");
-
-            if (this.id === deletedId || this.parent === deletedId) {
-                console.log("delete");
-                this.delete();
-                
-            }
-        });
-    }
-
-    delete() {
-
     }
 }
