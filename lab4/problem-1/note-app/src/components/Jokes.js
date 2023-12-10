262898a5beeea4fce49230aca60274d5873518cc
DDavitaa <c20406272@mytudublin.ie>
2023-12-10 16:53:01 +0000

    view punchlines to joke working

--- lab4/problem-1/note-app/src/components/Jokes.js
+++ lab4/problem-1/note-app/src/components/Jokes.js
@@ -3,40 +3,50 @@ import { from, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
 const Jokes = () => {
-  const [data, setData] = useState(null);
-  const [click, setClick] = useState(0);
-  const cancelFetch$ = useRef(new Subject());
+    const [data, setData] = useState(null);
+    const [click, setClick] = useState(0);
+    const [showPunchline, setShowPunchline] = useState(false);
+    const cancelFetch$ = useRef(new Subject());
 
-  useEffect(() => {
-    from(fetch('https://official-joke-api.appspot.com/random_joke'))
-      .pipe(takeUntil(cancelFetch$.current))
-      .subscribe(
-        async response => {
-          const data = await response.json();
-          setData(data);
-        },
-        error => console.error(error)
-      );
+    useEffect(() => {
+        setShowPunchline(false);
+        from(fetch('https://official-joke-api.appspot.com/random_joke'))
+        .pipe(takeUntil(cancelFetch$.current))
+        .subscribe(
+            async response => {
+            const data = await response.json();
+            setData(data);
+            },
+            error => console.error(error)
+        );
 
-    return () => {
-      cancelFetch$.current.next();
+        return () => {
+        cancelFetch$.current.next();
+        };
+    }, [click]);
+
+    const handleRandomizeClick = () => {
+        setClick(click + 1);
     };
-  }, [click]);
 
-  const handleClick = () => {
-    setClick(click + 1);
-  };
+    const handlePunchlineClick = () => {
+        setShowPunchline(true);
+    };
 
-  return (
-    <div>
-      {data ? (
-        <div>{data.setup}</div>
-      ) : (
-        <div>Loading...</div>
-      )}
-      <button onClick={handleClick}>Randomize Joke</button>
-    </div>
-  );
+    return (
+        <div>
+        {data ? (
+            <div>
+            <div>{data.setup}</div>
+            {showPunchline && <div>{data.punchline}</div>}
+            <button onClick={handlePunchlineClick}>Show Punchline</button>
+            </div>
+        ) : (
+            <div>Loading...</div>
+        )}
+        <button onClick={handleRandomizeClick}>Randomize Joke</button>
+        </div>
+    );
 };
 
 export default Jokes;
